889fa6142c39c8837d05f8273632363fd8a5349a
duanmuchaoqun <2947824360@qq.com>
2020-04-19 22:15:15 +0800

    源码学习

--- src/main/java/com/duanmu/springboot/bean/User.java
+++ src/main/java/com/duanmu/springboot/bean/User.java
@@ -3,9 +3,8 @@ package com.duanmu.springboot.bean;
 import lombok.Data;
 import lombok.ToString;
 import javax.validation.constraints.NotBlank;
+import java.util.Objects;
 
-@Data
-@ToString
 public class User {
 
     private Integer id;
@@ -18,4 +17,72 @@ public class User {
 
     @NotBlank(message = "电话号码不能为空")
     private String mobile;
+
+    public User() {
+    }
+
+    public User(Integer id, @NotBlank(message = "姓名不能为空") String name, @NotBlank(message = "邮箱地址不能为空") String email, @NotBlank(message = "电话号码不能为空") String mobile) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(mobile, user.mobile);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, mobile);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                '}';
+    }
 }
--- src/test/java/com/duanmu/springboot/SpringbootApplicationTests.java
+++ src/test/java/com/duanmu/springboot/SpringbootApplicationTests.java
@@ -1,11 +1,14 @@
 package com.duanmu.springboot;
 
+import com.duanmu.springboot.bean.User;
 import com.duanmu.springboot.common.context.MyContext;
 import com.duanmu.springboot.common.impl.BackDoor;
 import com.duanmu.springboot.common.impl.BlackEnemy;
 import com.duanmu.springboot.common.impl.GivenGreenLight;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Objects;
 
 @SpringBootTest
 class SpringbootApplicationTests {
@@ -31,5 +34,27 @@ class SpringbootApplicationTests {
         context.setStrategy(new BlackEnemy());
         context.operate();
         System.out.println("\n");
+    }
+
+    @Test
+    public void testHashCode(){
+        User user1 = new User();
+        User user2 = new User();
+        System.out.println(user1 == user2);
+        System.out.println(user1.equals(user2));
+        System.out.println(Objects.equals(user1,user2));
+        int hashCode = Objects.hash(1, "端木", "test@qq.com", "15258794569");
+        int hashCode2 = Objects.hash(1, "端木", "test@qq.com", "15258794569");
+        System.out.println(hashCode);
+        System.out.println(hashCode2);
+        User user3 = new User(1, "111", "test@qq.com", "15258794569");
+        int hashCode1 = user3.hashCode();
+        System.out.println(hashCode1);
+        int hashCode3 = Objects.hashCode(user3);
+        System.out.println(hashCode3);
+        String userStr = Objects.toString(null);
+        String user1Str = Objects.toString(user3);
+        System.out.println(userStr);
+        System.out.println(user1Str);
     }
 }
